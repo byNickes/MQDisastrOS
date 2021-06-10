43bc4ec70418cce13185e0fe824c1b126cf8b71b
Nicolò Baroncini <baroncini.1834907@studenti.uniroma1.it>
2021-06-10 19:24:10 +0200

    restored stack_size at previous value

--- disastrOS_constants.h
+++ disastrOS_constants.h
@@ -7,8 +7,7 @@
 #define MAX_NUM_TYPE_RESOURCES 1 //used to alloc resource_alloc_funct and resource_free_func
 #define MAX_MESSAGES_FOR_MQ 128 //max messages that can be stored in 1 MQ
 
-//#define STACK_SIZE        16384
-#define STACK_SIZE        80000
+#define STACK_SIZE        16384
 // signals
 #define MAX_SIGNALS 32
 #define DSOS_SIGCHLD 0x1
